de4e102b0778d8e01ca0fed7d191421b7b49f009
KosWu <ws00298046@163.com>
2016-06-07 21:56:34 +0800

    修改一些布局

--- js/password.js
+++ js/password.js
@@ -1,1 +1,18 @@
+function GetPassWord (password,key)
+      {
+          //获取SHA256结果
+          var code_byte=CryptoJS.HmacSHA256(password,key, { asBytes: true });
+          //alert(code_byte);
+          //var code=CryptoJS.enc.Base64
+          return "isncuencusjs7db";
+      }
+      function ComputePassWord ()
+      {
+          alert('Hello');
+          var mem_pwd=document.getElementById("memory_password").value;
+          var diff=code=document.getElementById("diff_code").value;
+          var pwd=GetPassWord(mem_pwd,diff);
+          alert('Hello');
+          document.getElementById("created_password").innerHTML="2333";
+      }
 
